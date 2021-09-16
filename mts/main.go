814e162416048e6ebfee8ae341555996a88c6e7f
militska <cheshirenok@gmail.com>
2021-09-16 22:41:06 +0500

    Задачки

--- mts/main.go
+++ mts/main.go
@@ -7,23 +7,17 @@ import (
 
 func Counter(str string) string {
 	var inc int
-	var key string
 	res := make(map[string]int)
 	runStr := []rune(str)
 	for inc = 0; inc < len([]rune(str)); inc++ {
-		key = string(runStr[inc])
-		if _, ok := res[key]; ok {
-			res[key]++
-			continue
-		}
-		res[key] = 1
+		res[string(runStr[inc])]++
 	}
 	SortKey := make([]string, 0, len(res))
 	for k := range res {
 		SortKey = append(SortKey, k)
 	}
 	sort.Strings(SortKey)
-	itog := ""
+	var itog string
 	for _, k := range SortKey {
 		itog += fmt.Sprintf("%s%d", k, res[k])
 	}
